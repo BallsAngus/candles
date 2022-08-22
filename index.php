d3be9c88a321fa22520f4c5f03640d9f7d0a1655
Julian Tuazon <jmtuazon09@gmail.com>
2022-08-22 04:06:19 -0700

    reworked to php for server side, front end item test

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+require_once('./php/component.php')
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="assets/styles.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.css" integrity="sha512-9nqhm3FWfB00id4NJpxK/wV1g9P2QfSsEPhSSpT+6qrESP6mpYbTfpC+Jvwe2XY27K5mLwwrqYuzqMGK5yC9/Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
@@ -13,7 +19,7 @@
         <nav class="navigation">
             <ul class="top_nav">
                 <li>
-                    <a href="front.html">HOME</a>
+                    <a href="index.php">HOME</a>
                 </li>
                 <li>
                     <a href="">FEATURED</a>
@@ -30,14 +36,15 @@
    <div class="logo">
     <img src="https://p1-e6eeae93.imageflux.jp/c!/a=2,w=788,u=0/harapecostoreinc/83e17412d61629c396a7.png">
    </div>
-   <div class="logo">
-    <img src="https://p1-e6eeae93.imageflux.jp/c!/a=2,w=788,u=0/harapecostoreinc/83e17412d61629c396a7.png">
-   </div>
-   <div class="logo">
-    <img src="https://p1-e6eeae93.imageflux.jp/c!/a=2,w=788,u=0/harapecostoreinc/83e17412d61629c396a7.png">
-   </div>
-   <div class="logo">
-    <img src="https://p1-e6eeae93.imageflux.jp/c!/a=2,w=788,u=0/harapecostoreinc/83e17412d61629c396a7.png">
+   <div class="container">
+    <div class="row text-center py-5">
+        <?php
+            component("Candle", "$9.99", "https://cdn.discordapp.com/attachments/460191223601430531/1007659107299377172/Screenshot_20220812-073543_Instagram.jpg");
+            component("Candle Bundle", "$19.99", "https://cdn.discordapp.com/attachments/460191223601430531/1007659107655876679/Screenshot_20220812-073553_Instagram.jpg");
+            component("Blossom Candle", "$12.99", "https://cdn.discordapp.com/attachments/460191223601430531/1007659107978850444/Screenshot_20220812-073613_Instagram.jpg");
+            component("Mini Bear Candle", "$5.99", "https://cdn.discordapp.com/attachments/460191223601430531/1007659108297609276/Screenshot_20220812-073624_Instagram.jpg");
+        ?>
+    </div>
    </div>
    <footer class="end">
     <div class="end_div"><p>COPYRIGHT</p></div>
---
+++ php/component.php
@@ -0,0 +1,25 @@
+<?php 
+
+function component($productname, $productprice, $productimg) {
+    $element="
+    
+    <div class=\"col-md-3 col-sm-6 my-3 my-md-0\">
+        <form action=\"index.php\" method=\"post\">
+            <div class=\"card shadow\">
+                <div>
+                    <img src=\"$productimg\" class=\"img-fluid card-img-top\">
+                </div>
+                <div class=\"card-body\">
+                    <h5 class=\"card-title\">$productname</h5>
+                    <p class=\"card-text\">Classic Candle</p>
+                    <h5 class=\"price\">$productprice</h5>
+                    <button type=\"submit\" class=\"btn btn-dark my-3\" name=\"add\">Add to Cart <i class=\"fas fa-shopping-cart\"></i></button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    ";
+
+    echo $element;
+}
